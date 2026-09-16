e59e264367677e7cf8e1ce57e50d7f356dcda079
Zach Wehbe <64331241+zachwehbe26@users.noreply.github.com>
2026-09-16 15:01:30 -0500

    Update sh_pat_handler.lua

--- ttt2-role_patient/lua/terrortown/autorun/client/sh_pat_handler.lua
+++ ttt2-role_patient/lua/terrortown/autorun/client/sh_pat_handler.lua
@@ -1,8 +1,15 @@
 AddCSLuaFile()
+PATIENT_DATA = {}
+PATIENT_DATA.playersInfected = {}
+
+
+function patAddPlayerInfected( ply )
+    print("Adding player ",ply:Nick()," to the infected table ")
+    table.insert(playersInfected, ply)
+end
 
 
 net.Receive("ttt2_pat_infect", function()
     EPOP:AddMessage({text =  LANG.TryTranslation("lang_pat_infect_title"), color = PATIENT.color}, {text = LANG.TryTranslation("lang_pat_infect_desc")}, 5, nil, true)
 end)
-
 
